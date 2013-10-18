db308e5afad1b8b755ee925b4f86212c0f546260
goggle1 <goggle1@163.com>
2013-10-18 17:24:03 +0800

    first commit

---
+++ channel.cpp
@@ -0,0 +1,439 @@
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "channel.h"
+
+void source_release(void* datap)
+{
+	if(datap == NULL)
+	{
+		return;
+	}
+
+	SOURCE_T* sourcep = (SOURCE_T*)datap;
+	// do nothing.
+	
+	free(sourcep);
+}
+
+
+void channel_release(void* datap)
+{
+	if(datap == NULL)
+	{
+		return;
+	}
+	
+	CHANNEL_T* channelp = (CHANNEL_T*)datap;
+	if(channelp->source_list != NULL)
+	{
+		deque_release(channelp->source_list, source_release);	
+	}
+	
+	free(channelp);
+}
+
+ChannelList::ChannelList()
+{
+	m_channel_list = NULL;
+}
+
+ChannelList::~ChannelList()
+{
+	if(m_channel_list != NULL)
+	{
+		deque_release(m_channel_list, channel_release);
+		m_channel_list = NULL;
+	}
+}
+
+int ChannelList::ParseChannel(xmlDocPtr doc, xmlNodePtr cur, CHANNEL_T* channelp)
+{	
+	xmlAttrPtr attrPtr = cur->properties;
+	while (attrPtr != NULL)
+	{
+		if (!xmlStrcmp(attrPtr->name, BAD_CAST "channel_id"))
+		{
+			xmlChar* szAttr = xmlGetProp(cur, BAD_CAST "channel_id");		   
+			channelp->channel_id = atoi((const char*)szAttr);
+			xmlFree(szAttr);
+		}
+		else if (!xmlStrcmp(attrPtr->name, BAD_CAST "liveid"))
+		{
+			xmlChar* szAttr = xmlGetProp(cur, BAD_CAST "liveid");		   
+			strncpy(channelp->liveid, (const char*)szAttr, MAX_LIVE_ID-1);
+			channelp->liveid[MAX_LIVE_ID-1] = '\0';
+			xmlFree(szAttr);
+		}
+		else if (!xmlStrcmp(attrPtr->name, BAD_CAST "bitrate"))
+		{
+			xmlChar* szAttr = xmlGetProp(cur, BAD_CAST "bitrate");		   
+			channelp->bitrate = atoi((const char*)szAttr);
+			xmlFree(szAttr);
+		}
+		else if (!xmlStrcmp(attrPtr->name, BAD_CAST "channel_name"))
+		{
+			xmlChar* szAttr = xmlGetProp(cur, BAD_CAST "channel_name");		   
+			strncpy(channelp->channel_name, (const char*)szAttr, MAX_CHANNEL_NAME-1);
+			channelp->channel_name[MAX_CHANNEL_NAME-1] = '\0';
+			xmlFree(szAttr);
+		}
+		else if (!xmlStrcmp(attrPtr->name, BAD_CAST "codec_ts"))
+		{
+			xmlChar* szAttr = xmlGetProp(cur, BAD_CAST "codec_ts");		   
+			channelp->codec_ts = atoi((const char*)szAttr);
+			xmlFree(szAttr);
+		}
+		else if (!xmlStrcmp(attrPtr->name, BAD_CAST "codec_flv"))
+		{
+			xmlChar* szAttr = xmlGetProp(cur, BAD_CAST "codec_flv");		   
+			channelp->codec_flv = atoi((const char*)szAttr);
+			xmlFree(szAttr);
+		}
+		else if (!xmlStrcmp(attrPtr->name, BAD_CAST "codec_mp4"))
+		{
+			xmlChar* szAttr = xmlGetProp(cur, BAD_CAST "codec_mp4");		   
+			channelp->codec_mp4 = atoi((const char*)szAttr);
+			xmlFree(szAttr);
+		}
+
+		attrPtr = attrPtr->next;
+	}
+
+	DEQUE_NODE* source_list = NULL;	
+	xmlNodePtr child;	
+	child = cur->xmlChildrenNode;
+	while((child != NULL) && (child->name != NULL))
+	{
+		if((!xmlStrcmp(child->name, (const xmlChar*)"sources")))
+		{			
+			source_list = ParseSources(doc, child);
+			if(source_list != NULL)
+			{
+				channelp->source_list = deque_link(channelp->source_list, source_list);
+			}
+		}
+		
+		child = child->next;
+	}
+	
+
+	return 0;
+}
+
+DEQUE_NODE* ChannelList::ParseChannels(xmlDocPtr doc, xmlNodePtr cur)
+{	
+	DEQUE_NODE* headp = NULL;
+	
+	xmlNodePtr child;	
+	child = cur->xmlChildrenNode;
+	while((child != NULL) && (child->name != NULL))
+	{
+		if((!xmlStrcmp(child->name, (const xmlChar*)"channel")))
+		{
+			DEQUE_NODE* nodep = (DEQUE_NODE*)malloc(sizeof(DEQUE_NODE));
+			if(nodep == NULL)
+			{
+			    fprintf(stderr, "%s: can not malloc for DEQUE_NODE\n", __FUNCTION__);
+			    return headp;
+			}
+			memset(nodep, 0, sizeof(DEQUE_NODE));
+			
+			CHANNEL_T* channelp = NULL;
+			channelp = (CHANNEL_T*)malloc(sizeof(CHANNEL_T));
+			if(channelp == NULL)
+			{
+			    fprintf(stderr, "%s: can not malloc for CHANNEL_T\n", __FUNCTION__);
+			    free(nodep);
+			    return headp;
+			}
+			memset(channelp, 0, sizeof(CHANNEL_T));
+			nodep->datap = channelp;
+			nodep->prevp = NULL;
+			nodep->nextp = NULL;			
+			
+			int ret = ParseChannel(doc, child, channelp);
+			if(ret == 0)
+			{		
+            	headp = deque_append(headp, nodep);
+            }
+            else
+            {
+		    	free(channelp);
+            	free(nodep);
+            }
+		}
+		
+		child = child->next;
+	}
+	
+	return headp;
+}
+
+
+DEQUE_NODE* ChannelList::ParseSources(xmlDocPtr doc, xmlNodePtr cur)
+{	
+	DEQUE_NODE* headp = NULL;
+	
+	xmlNodePtr child;	
+	child = cur->xmlChildrenNode;
+	while((child != NULL) && (child->name != NULL))
+	{
+		if((!xmlStrcmp(child->name, (const xmlChar*)"source")))
+		{
+			DEQUE_NODE* nodep = (DEQUE_NODE*)malloc(sizeof(DEQUE_NODE));
+			if(nodep == NULL)
+			{
+			    fprintf(stderr, "%s: can not malloc for DEQUE_NODE\n", __FUNCTION__);
+			    return headp;
+			}
+			memset(nodep, 0, sizeof(DEQUE_NODE));
+			
+			SOURCE_T* sourcep = NULL;
+			sourcep = (SOURCE_T*)malloc(sizeof(SOURCE_T));
+			if(sourcep == NULL)
+			{
+			    fprintf(stderr, "%s: can not malloc for SOURCE_T\n", __FUNCTION__);
+			    free(nodep);
+			    return headp;
+			}
+			memset(sourcep, 0, sizeof(SOURCE_T));
+			nodep->datap = sourcep;
+			nodep->prevp = NULL;
+			nodep->nextp = NULL;			
+			
+			int ret = ParseSource(doc, child, sourcep);
+			if(ret == 0)
+			{		
+            	headp = deque_append(headp, nodep);
+            }
+            else
+            {
+		    	free(sourcep);
+            	free(nodep);
+            }
+		}
+		
+		child = child->next;
+	}
+	
+	return headp;
+}
+
+int ChannelList::ParseSource(xmlDocPtr doc, xmlNodePtr cur, SOURCE_T* sourcep)
+{	
+	xmlAttrPtr attrPtr = cur->properties;
+	while (attrPtr != NULL)
+	{
+		if (!xmlStrcmp(attrPtr->name, BAD_CAST "ip"))
+		{
+			xmlChar* szAttr = xmlGetProp(cur, BAD_CAST "ip");		   
+			sourcep->ip = inet_network((const char*)szAttr);
+			xmlFree(szAttr);
+		}
+		else if (!xmlStrcmp(attrPtr->name, BAD_CAST "port"))
+		{
+			xmlChar* szAttr = xmlGetProp(cur, BAD_CAST "port");		   
+			sourcep->port = atoi((const char*)szAttr);
+			xmlFree(szAttr);
+		}
+
+		attrPtr = attrPtr->next;
+	}
+
+	return 0;
+}
+
+int ChannelList::ReadConfig(char* config_file)
+{
+	xmlDocPtr doc;
+	xmlNodePtr cur;
+	
+	doc=xmlParseFile(config_file);
+	//doc = xmlParseMemory(xml_str.c_str(), xml_str.length());
+	if(doc == NULL)
+	{		
+		fprintf(stderr, "%s: xml parsed failed!\n", __FUNCTION__);
+		return -1;
+	}
+
+	cur = xmlDocGetRootElement(doc);
+	if(cur == NULL)
+	{
+		fprintf(stderr, "%s: empty document!\n", __FUNCTION__);
+		xmlFreeDoc(doc);
+		return -1;
+	}
+
+	if (xmlStrcmp(cur->name, (const xmlChar *)"channels") != 0)
+	{	
+		fprintf(stderr, "%s: root node is not 'channels'!\n", __FUNCTION__);
+		xmlFreeDoc(doc);
+		return -1;
+	}
+	
+	DEQUE_NODE* channel_list = ParseChannels(doc, cur);	
+	xmlFreeDoc(doc);
+
+    if(channel_list)
+    {
+	    m_channel_list = deque_link(m_channel_list, channel_list);
+	    return 0;
+    }
+
+	
+	return 0;
+}
+
+#if 0
+int ChannelList::WriteConfig(char* config_file)
+{
+	if(m_channel_list == NULL)
+	{
+		return -1;
+	}
+
+	xmlDocPtr doc = NULL;
+	xmlNodePtr channels_node = NULL;
+	xmlNodePtr channel_node = NULL;
+	xmlNodePtr sources_node = NULL;
+	xmlNodePtr source_node = NULL;
+	
+	doc = xmlNewDoc(BAD_CAST "1.0");
+	channels_node = xmlNewNode(NULL, BAD_CAST "channels");
+	xmlDocSetRootElement(doc, channels_node);
+
+	DEQUE_NODE* nodep = m_channel_list;
+	while(nodep)
+	{
+		CHANNEL_T* channelp = (CHANNEL_T*)nodep->datap;
+		channel_node = xmlNewChild(channels_node, NULL, BAD_CAST "channel", BAD_CAST"");
+
+		char str_channel_id[64] = {'\0'};
+		sprintf(str_channel_id, "%d", channelp->channel_id);
+		xmlNewProp(channel_node, BAD_CAST "channel_id", BAD_CAST str_channel_id);
+		xmlNewProp(channel_node, BAD_CAST "liveid", BAD_CAST channelp->liveid);
+		char str_bitrate[64] = {'\0'};
+		sprintf(str_bitrate, "%d", channelp->bitrate);
+		xmlNewProp(channel_node, BAD_CAST "bitrate", BAD_CAST str_bitrate);
+		xmlNewProp(channel_node, BAD_CAST "channel_name", BAD_CAST channelp->channel_name);
+		char str_codec[64] = {'\0'};
+		sprintf(str_codec, "%d", channelp->codec_ts);
+		xmlNewProp(channel_node, BAD_CAST "codec_ts", BAD_CAST str_codec);
+		sprintf(str_codec, "%d", channelp->codec_flv);
+		xmlNewProp(channel_node, BAD_CAST "codec_flv", BAD_CAST str_codec);
+		sprintf(str_codec, "%d", channelp->codec_mp4);
+		xmlNewProp(channel_node, BAD_CAST "codec_mp4", BAD_CAST str_codec);
+
+		sources_node = xmlNewChild(channel_node, NULL, BAD_CAST "sources", BAD_CAST"");		
+		DEQUE_NODE* node2p = channelp->source_list;
+		while(node2p)
+		{
+			SOURCE_T* sourcep = (SOURCE_T*)node2p->datap;
+			source_node = xmlNewChild(sources_node, NULL, BAD_CAST "source", BAD_CAST"");	
+
+			struct in_addr in;
+			in.s_addr = htonl(sourcep->ip);
+			char* str_ip = inet_ntoa(in);
+			xmlNewProp(source_node, BAD_CAST "ip", BAD_CAST str_ip);
+			char str_port[64] = {'\0'};
+			sprintf(str_port, "%d", sourcep->port);
+			xmlNewProp(source_node, BAD_CAST "port", BAD_CAST str_port);
+			
+			if(node2p->nextp == channelp->source_list)
+			{
+				break;
+			}			
+			node2p = node2p->nextp;
+		}
+		
+		if(nodep->nextp == m_channel_list)
+		{
+			break;
+		}
+		nodep = nodep->nextp;
+	}
+	
+	//Dumping document to stdio or file
+	xmlSaveFormatFileEnc(config_file, doc, "UTF-8", 1);
+	/*free the document */
+	xmlFreeDoc(doc);
+	xmlCleanupParser();
+	//xmlMemoryDump();//debug memory for regression tests
+	
+	return 0;
+}
+#endif
+
+int ChannelList::WriteConfig(char* config_file)
+{
+	if(m_channel_list == NULL)
+	{
+		return -1;
+	}
+
+	FILE* filep = fopen(config_file, "w");
+	if(filep == NULL)
+	{
+		return -1;
+	}
+
+	fprintf(filep, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+	fprintf(filep, "<channels>\n");
+
+	DEQUE_NODE* nodep = m_channel_list;
+	while(nodep)
+	{
+		CHANNEL_T* channelp = (CHANNEL_T*)nodep->datap;
+		
+		fprintf(filep, "\t<channel channel_id=\"%d\" liveid=\"%s\" bitrate=\"%d\" channel_name=\"%s\" "
+			"codec_ts=\"%d\" codec_flv=\"%d\" codec_mp4=\"%d\">\n", 
+			channelp->channel_id, channelp->liveid, channelp->bitrate, channelp->channel_name,
+			channelp->codec_ts, channelp->codec_flv, channelp->codec_mp4);
+
+		fprintf(filep, "\t\t<sources>\n");
+		DEQUE_NODE* node2p = channelp->source_list;
+		while(node2p)
+		{
+			SOURCE_T* sourcep = (SOURCE_T*)node2p->datap;
+
+			struct in_addr in;
+			in.s_addr = htonl(sourcep->ip);
+			char* str_ip = inet_ntoa(in);
+			
+			fprintf(filep, "\t\t\t<source ip=\"%s\" port=\"%d\">\n",
+				str_ip, sourcep->port);
+			fprintf(filep, "\t\t\t</source>\n");
+			
+			if(node2p->nextp == channelp->source_list)
+			{
+				break;
+			}			
+			node2p = node2p->nextp;
+		}
+		fprintf(filep, "\t\t</sources>\n");
+			
+		fprintf(filep, "\t</channel>\n");		
+		
+		if(nodep->nextp == m_channel_list)
+		{
+			break;
+		}
+		nodep = nodep->nextp;
+	}
+	
+	fprintf(filep, "</channels>\n");
+
+	if(filep != NULL)
+	{
+		fclose(filep);
+		filep = NULL;
+	}
+	
+	return 0;
+}
+
+
+
---
+++ channel.h
@@ -0,0 +1,56 @@
+
+#ifndef _CHANNEL_H__
+#define _CHANNEL_H__
+
+#include <libxml/xmlmemory.h>
+#include <libxml/parser.h>
+
+#include "deque.h"
+
+#define HASH_LEN 			40
+#define MAX_LIVE_ID     	44
+#define MAX_CHANNEL_NAME	64
+
+typedef struct source_t
+{
+	u_int32_t	ip;
+	u_int16_t	port;
+} SOURCE_T;
+
+//{"channel_id":"21","LOWER(t.liveid)":"0b49884b3b85f7ccddbe4e96e4ae2eae7a6dec56","bitrate":"800","channel_name":"\u4e1c\u65b9\u536b\u89c6"},
+typedef struct channel_t
+{
+	int 	channel_id;	
+	char 	liveid[MAX_LIVE_ID];
+	int 	bitrate;
+	char	channel_name[MAX_CHANNEL_NAME];
+	int	  	codec_ts; 	// 1: on, 0: off
+	int		codec_flv;	// 1: on, 0: off
+	int		codec_mp4;	// 1: on, 0: off
+	DEQUE_NODE* source_list;
+} CHANNEL_T;
+
+class ChannelList
+{
+	public:
+		ChannelList();
+		~ChannelList();
+
+		int			ReadConfig(char* config_file);
+		int			WriteConfig(char* config_file);
+
+		CHANNEL_T* 	FindChannel(char* liveid);
+		int			AddChannel(CHANNEL_T* channelp);
+		int			DeleteChannel(char* liveid);
+		DEQUE_NODE* GetChannels() { return m_channel_list; };
+		
+	protected:
+		DEQUE_NODE* ParseChannels(xmlDocPtr doc, xmlNodePtr cur);
+		int			ParseChannel(xmlDocPtr doc, xmlNodePtr cur, CHANNEL_T* channelp);
+		DEQUE_NODE* ParseSources(xmlDocPtr doc, xmlNodePtr cur);
+		int			ParseSource(xmlDocPtr doc, xmlNodePtr cur, SOURCE_T* sourcep);
+		
+		DEQUE_NODE*	m_channel_list;		
+};
+
+#endif
