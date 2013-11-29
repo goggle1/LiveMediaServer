65c13dbd3c6acf7054a287b47e6253a106d7f784
goggle1 <goggle1@163.com>
2013-11-29 17:02:39 +0800

    build

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ make
 cp -rf LiveMediaServer ./bin/ 
 cp -rf LiveMediaServer.xml ./bin/
 cp -rf ./html/ ./bin/
-#tar -czvf  LiveMediaServer-0.1.0.0.tar.gz ./bin/ 
+#tar -czvf  LiveMediaServer-0.1.0.003.tar.gz ./bin/ 
