d1bb38f236e95d87464e20a17501313191030085
goggle1 <goggle1@163.com>
2013-12-26 15:01:44 +0800

    analyzer

---
+++ analyzer/DelayAnalyzer.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+import sys
+import string
+import time
+import datetime
+
+def print_usage(program_name):
+    print '%s [log_file]' % (program_name)
+
+
+def file_get_key(file_name):
+    #/livestream/3702892333/78267cf4a7864a887540cf4af3c432dca3d52050/flv/2013/12/25/20131017T171949_03_20131225_153710_3509995.flv
+    items = file_name.split('/')
+    only_name = items[8]
+    parts = only_name.split('_')
+    part_date = parts[2]
+    part_time = parts[3]
+    part_id = parts[4]
+    key = "%s_%s_%s" % (part_date, part_time, part_id)
+    return key
+    
+        
+def file_get_time(file_name):
+    #/livestream/3702892333/78267cf4a7864a887540cf4af3c432dca3d52050/flv/2013/12/25/20131017T171949_03_20131225_153710_3509995.flv
+    items = file_name.split('/')
+    only_name = items[8]
+    parts = only_name.split('_')
+    part_date = parts[2]
+    part_time = parts[3]
+    part_id = parts[4]
+    year = part_date[0:4]
+    month = part_date[4:6]
+    day = part_date[6:8]    
+    i_year = string.atoi(year)
+    i_month = string.atoi(month)
+    i_day = string.atoi(day)
+    #print '%s, %s, %s' % ( year, month, day )
+    #print '%d, %d, %d' % ( i_year, i_month, i_day )
+    hour = part_time[0:2]
+    minute = part_time[2:4]
+    second = part_time[4:6]
+    i_hour = string.atoi(hour)
+    i_minute = string.atoi(minute)
+    i_second = string.atoi(second)
+    #print '%s, %s, %s' % ( hour, minute, second )
+    #print '%d, %d, %d' % ( i_hour, i_minute, i_second )    
+    dt = datetime.datetime(i_year, i_month, i_day, i_hour, i_minute, i_second)
+    #print dt.timetuple()
+    timestamp = time.mktime(dt.timetuple())
+    #print timestamp
+    return timestamp
+
+def section_get_time(section):
+    #begin: 1387957182.174209 [Wed Dec 25 15:39:42 2013]
+    items = section.split(' ')
+    return items[2]
+
+def main():
+    if(len(sys.argv) < 2):
+        print_usage(sys.argv[0])
+        return False
+        
+    file_name = sys.argv[1]    
+    log_file = open(file_name, 'r')
+    
+    while(True):
+        line = log_file.readline()
+        if(line == ''):
+            break           
+        items = line.split(',')        
+        if(len(items) < 4):
+            continue
+        section_file        = items[0]
+        section_len         = items[1]
+        section_begin_time  = items[2]
+        section_end_time    = items[3]        
+        segment_key = file_get_key(section_file)
+        segment_timestamp = file_get_time(section_file)
+        begin_time = section_get_time(section_begin_time)
+        end_time = section_get_time(section_end_time)
+        begin_timestamp = string.atof(begin_time)
+        end_timestamp = string.atof(end_time)
+        download_time = end_timestamp - begin_timestamp
+        delay_time = end_timestamp - segment_timestamp
+        print '%s, %f, %f, %f, %f, %f' % (segment_key, segment_timestamp, begin_timestamp, end_timestamp, download_time, delay_time)          
+        
+    return True
+
+    
+if __name__ == '__main__':
+    main()  
+        
+        
---
+++ html/query_session.xsl
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+<xsl:template match="/">
+<html>
+<body>
+<h2>Query Session [<xsl:value-of select="sessions/@ip" />]:</h2>
+<table cellspacing="0" cellpading="0" bordercolor="#cccccc" border="1">
+<tr bgcolor="#9acd32">
+<th align="center">remote_ip</th>
+<th align="center">remote_port</th>
+<th align="center">session_type</th>
+<th align="center">upload_bytes</th>
+<th align="center">download_bytes</th>
+<th align="center">begin_time</th>
+<th align="center">end_time</th>
+<th align="center">now_time</th>
+<th align="center">upload_rate</th>
+<th align="center">download_rate</th>
+</tr>
+
+<xsl:for-each select="sessions/session">
+<tr>
+	<xsl:if test="position() mod 2 = 1">
+		<xsl:attribute name="bgcolor">#EEEEEE</xsl:attribute>
+	</xsl:if>
+<td align="left"><xsl:value-of select="@remote_ip" /></td>
+<td align="right"><xsl:value-of select="@remote_port" /></td>
+<td align="right"><xsl:value-of select="@session_type" /></td>
+<td align="right"><xsl:value-of select="@upload_bytes" /></td>
+<td align="right"><xsl:value-of select="@download_bytes" /></td>
+<td align="right"><xsl:value-of select="@begin_time" /></td>
+<td align="right"><xsl:value-of select="@end_time" /></td>
+<td align="right"><xsl:value-of select="@now_time" /></td>
+<td align="right"><xsl:value-of select="@upload_rate" /></td>
+<td align="right"><xsl:value-of select="@download_rate" /></td>
+</tr>
+
+</xsl:for-each>
+</table>
+
+</body>
+</html>
+</xsl:template>
+
+</xsl:stylesheet>
