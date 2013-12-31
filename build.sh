859af93fb09eca900cf0ef2991b7a1d95261ef0a
goggle1 <goggle1@163.com>
2013-12-31 17:48:15 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 export OS_VERSION=`cat /etc/redhat-release|awk '{print $1$3}'`
 make clean
 make
+rm -rf ./bin/*
 cp -rf LiveMediaServer ./bin/ 
 #tar -czvf  LiveMediaServer-0.1.0.022.tar.gz ./bin/ ./etc/ ./html/
